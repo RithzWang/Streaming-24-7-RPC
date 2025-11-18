2092c438ef735cf46bf12c3c49f64ebe7919c99f
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-18 20:48:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on("ready", async () => {
     .setApplicationId("1440267761581293659")
     .setType("STREAMING")
     .setURL("https://youtu.be/_LTjhKM8tvs") //Must be a youtube video link
-    .setState("اللهم صل وسلم وبارك على نبينا محمد")
+    // .setState("اللهم صل وسلم وبارك على نبينا محمد")
     .setName("Is this really love?")
     .setDetails(`${formatTime()} (UTC+7)`)
     .setStartTimestamp(Date.now())
