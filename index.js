55cf316942e0998e9530fe04ab3c62e2262fb3dd
RithzWang <101114513+RithzWang@users.noreply.github.com>
2025-11-18 21:48:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ client.on("ready", async () => {
     .setApplicationId("1440267761581293659")
     .setType("STREAMING")
     .setURL("https://youtu.be/_LTjhKM8tvs") //Must be a youtube video link
-     .setState("مادري وش ذا")
+     .setState("🤎")
     .setName("Is this really love?")
     .setDetails(`${formatTime()} (UTC+7)`)
     .setStartTimestamp(Date.now())
